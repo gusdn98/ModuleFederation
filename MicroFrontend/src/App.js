06cbcce9371a5fb1d8a7b17e0cd934f8f167d3b5
gusdn98 <ah529@naver.com>
2024-01-12 14:38:25 +0900

    moduleFederation 연습

--- MicroFrontend/src/App.js
+++ MicroFrontend/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 const Button = React.lazy(() => import("MicroFrontend/Button"));
-
+//"webpack.config.js(가져올앱의 별칭)"/"expose한 모듈의 이름"
 export default function App() {
   return (
     <div>
