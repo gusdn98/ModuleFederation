7fe91f06796a1a37aafa629d10a2b21533f1ca18
gusdn98 <ah529@naver.com>
2024-04-26 10:39:25 +0900

    main

--- microfrontend-1/src/Button.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Button({ buttonName }) {
-  return (
-    <button onClick={console.log("microfrontend의 버튼이에요")}>
-      {buttonName}
-    </button>
-  );
-}
